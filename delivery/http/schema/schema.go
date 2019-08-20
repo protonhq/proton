4a6c8b72cd3197a5c823c4860b19afb0873a4294
Yeung Yiu Hung <hkclex@gmail.com>
2019-08-20 17:28:51 +0800

    feat(graphql): add create account endpoint

--- delivery/http/schema/schema.go
+++ delivery/http/schema/schema.go
@@ -3,15 +3,23 @@ package schema
 import "github.com/graphql-go/graphql"
 
 var rootQuery = graphql.NewObject(graphql.ObjectConfig{
-	Name: "RootQuery",
+	Name:        "RootQuery",
 	Description: "Root Query",
 	Fields: graphql.Fields{
 		"account": &queryAccount,
 	},
 })
 
+var rootMutation = graphql.NewObject(graphql.ObjectConfig{
+	Name:        "RootMutation",
+	Description: "Root Mutation",
+	Fields: graphql.Fields{
+		"create_account": &createAccount,
+	},
+})
+
 // Schema - graphql schema
 var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
 	Query:    rootQuery,
-	Mutation: nil,
+	Mutation: rootMutation,
 })
