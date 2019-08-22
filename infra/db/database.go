ca19a0d8eac69c9cd505ca2b08c8cbec65eb12b1
Yeung Yiu Hung <hkclex@gmail.com>
2019-08-22 11:19:19 +0800

    feat(account): add account usecase

--- infra/db/database.go
+++ infra/db/database.go
@@ -3,14 +3,14 @@ package db
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	
+
 	"github.com/protonhq/proton/domain"
 )
 
 // InitDB - Create Database Connection
-func InitDB(connection string) *gorm.DB, error {
+func InitDB(connection string) (*gorm.DB, error) {
 	db, err := gorm.Open("postgres", connection)
-	return &db, err
+	return db, err
 }
 
 // Migrate - Migrate Database
